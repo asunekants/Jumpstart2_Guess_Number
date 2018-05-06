28954712f545852e20a596258d3e5fdb0b68fb28
asunekants <13184677+asunekants@users.noreply.github.com>
2018-05-06 01:55:17 +0100

    Temp save 4

--- program.py
+++ program.py
@@ -10,10 +10,12 @@ the_number = random.randint(0, 100)
 guess_text = input('Guess a number between 0 and 100: ')
 guess = int(guess_text)
 
-if guess_text == guess:
-  print('equal')
-else:
-  print('unequal')
-
+def compareGuess(number, guess):
+  if number == guess:
+    print('You guessed correctly!')
+  elif number >= guess:
+    print('Your guess is HIGHER than the number; Guess again!')
+  else number >= guess:
+    print('Your guess is LOWER than the number; Guess again!')
 
 # TODO - continue work
