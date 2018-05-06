7a13b645f15b5f620389391bc564fdb70cfeebb9
asunekants <13184677+asunekants@users.noreply.github.com>
2018-05-06 01:14:46 +0100

    Initial commit

---
+++ program.py
@@ -0,0 +1,11 @@
+import random
+
+print('--------------------------------------')
+print('        GUESS THAT NUMBER GAME')
+print('--------------------------------------')
+print()
+
+the_number = random.randint(0, 100)
+
+guess_text = input('Guess a number between 0 and 100: ')
+
